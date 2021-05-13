657eaedde23314847843e9a1da7cf5dddadbc209
RahmaYasser <rahma.y9079@gmail.com>
2021-05-14 01:30:41 +0200

    updating users table

--- Backend-RubyOnRails/db/schema.rb
+++ Backend-RubyOnRails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_013324) do
+ActiveRecord::Schema.define(version: 2021_05_13_230343) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_013324) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
