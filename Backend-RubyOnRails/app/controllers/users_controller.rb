2c15a0a5526839b9dd96b67b3674e5cb71b9ac32
RahmaYasser <rahma.y9079@gmail.com>
2021-05-15 22:18:40 +0200

    users table and blogs completed, routes completed and signup completed

--- Backend-RubyOnRails/app/controllers/users_controller.rb
+++ Backend-RubyOnRails/app/controllers/users_controller.rb
@@ -22,16 +22,17 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = "welcome to FCAI-Blog, you have successfully signed up"
+      redirect_to blogs_path
+    else
+      render 'new'
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
   # PATCH/PUT /users/1 or /users/1.json
--- Backend-RubyOnRails/db/schema.rb
+++ Backend-RubyOnRails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_230343) do
+ActiveRecord::Schema.define(version: 2021_05_14_214245) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(version: 2021_05_13_230343) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "user_name"
+    t.string "password_digest"
   end
 
 end
